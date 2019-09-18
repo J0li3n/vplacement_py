2874f381b73be3b1b0101e1458b13dc2739927ed
jolien <jolien@xomnia.com>
2019-09-18 09:26:09 -0400

    flask api for showing json output

---
+++ vplacement/api.py
@@ -0,0 +1,29 @@
+import flask
+from flask import request, jsonify
+from dotenv import load_dotenv
+from ingestion.spoonacular import Spoonacular
+
+load_dotenv(dotenv_path='vplacement/config/.env')
+
+#Creating an object
+recipe = Spoonacular("https://www.delish.com/cooking/recipe-ideas/recipes/a57949/easy-shepherds-pie-recipe/")
+
+recipe.get_input_recipe()
+recipe.get_ingredients()
+recipe.get_output_recipes()
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Vplacement</h1>
+<p>Whoop, whoop, coolest webapp ever!! </p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/results', methods=['GET'])
+def api_all():
+    return recipe.output_recipes
+
+app.run()
--- vplacement/main.py
+++ vplacement/main.py
@@ -1,15 +1,14 @@
-import os
-from dotenv import load_dotenv
-from ingestion.spoonacular import Spoonacular
-
-load_dotenv(dotenv_path='vplacement/config/.env')
-
-#recipe = Spoonacular("https://www.ricardocuisine.com/en/recipes/5762-classic-beef-chili")
-#recipe = Spoonacular("https://natashaskitchen.com/fish-tacos-recipe/")
-recipe = Spoonacular("https://www.delish.com/cooking/recipe-ideas/recipes/a57949/easy-shepherds-pie-recipe/")
-
-recipe.get_input_recipe()
-recipe.get_ingredients()
-recipe.get_output_recipes()
-print(recipe.output_recipes)
-print(recipe.ingredients)
+# from dotenv import load_dotenv
+# from ingestion.spoonacular import Spoonacular
+#
+# load_dotenv(dotenv_path='vplacement/config/.env')
+#
+# #recipe = Spoonacular("https://www.ricardocuisine.com/en/recipes/5762-classic-beef-chili")
+# #recipe = Spoonacular("https://natashaskitchen.com/fish-tacos-recipe/")
+# recipe = Spoonacular("https://www.delish.com/cooking/recipe-ideas/recipes/a57949/easy-shepherds-pie-recipe/")
+#
+# recipe.get_input_recipe()
+# recipe.get_ingredients()
+# recipe.get_output_recipes()
+# print(recipe.output_recipes)
+# print(recipe.ingredients)
