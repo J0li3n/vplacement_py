b72077217f6ecd76eb21cb4aea4752d29ea20497
jolien <jolien@xomnia.com>
2019-10-03 10:50:39 -0400

    remove allergies from ingredient list

--- vplacement/ingestion/spoonacular.py
+++ vplacement/ingestion/spoonacular.py
@@ -9,7 +9,7 @@ load_dotenv(dotenv_path='config/.env')
 
 class Spoonacular:
 
-    def __init__(self, input_recipe_url, veg_option='vegetarian'):
+    def __init__(self, input_recipe_url, veg_option='vegetarian', allergies=None):
         self.API_KEY = os.getenv('API_KEY')
         self.input_recipe_url = input_recipe_url
         self.input_recipe = {}
@@ -18,6 +18,7 @@ class Spoonacular:
         self.veg_option = veg_option
         self.output_recipes = []
         self.output_recipes_veg = []
+        self.allergies = allergies
 
     def get_input_recipe(self):
         """
@@ -76,9 +77,13 @@ class Spoonacular:
         # Exclude meat and seafood from dataframe
         df = self.original_df[self.original_df['aisle'] != 'Meat']
         df = df[df['aisle'] != 'Seafood']
-        df = df[-((df['name'].str.contains('meat')) |
-                  (df['name'].str.contains('beef')) |
-                  (df['name'].str.contains('chicken')))]
+        meat_words = ['meat', 'beef', 'chicken']
+        df = df[-(df['name'].apply(lambda x: any(word in x for word in meat_words)))]
+
+        # Exclude allergies from dataframe
+        if self.allergies is not None:
+            allergies_list = self.allergies.split(",")
+            df = df[-(df['name'].apply(lambda x: any(word in x for word in allergies_list)))]
 
         if self.veg_option == 'vegan':
             df = df[-(df['aisle'].str.contains('Cheese'))]
@@ -107,6 +112,26 @@ class Spoonacular:
                 self.output_recipes_veg.append(self.output_recipes[recipe])
         self.output_recipes_veg = json.dumps({"output_recipes": self.output_recipes_veg})
         self.output_recipes_veg = json.loads(self.output_recipes_veg)
+
+    def remove_allergies(self):
+        result = []
+        result_ids = []
+        allergy_free_recipes = []
+        allergy_list = self.allergies.split(', ')
+        for recipe in range(0, len(self.output_recipes_veg)):
+            for ingredient in range(0, len(self.output_recipes_veg[recipe]['missedIngredients'])):
+                # Only continue when the recipe is not already found to have an allergy ingredient
+                if self.output_recipes_veg[recipe]['id'] not in result_ids:
+                    # Add recipe id to list if the recipe contains an allergy ingredient
+                    if any(word in self.output_recipes_veg[recipe]['missedIngredients'][ingredient]['name']
+                           for word in allergy_list):
+                        result.append(self.output_recipes_veg[recipe])
+                        result_ids.append(self.output_recipes_veg[recipe]['id'])
+            if self.output_recipes_veg[recipe]['id'] not in result_ids:
+                allergy_free_recipes.append(self.output_recipes_veg[recipe])
+        allergy_free_recipes = json.dumps({"output_recipes": allergy_free_recipes})
+        self.output_recipes_veg = json.loads(allergy_free_recipes)
+        return self.output_recipes_veg
 
 class RecipeId:
 
--- vplacement/ingestion/test.py
+++ vplacement/ingestion/test.py
@@ -9,17 +9,24 @@ load_dotenv(dotenv_path='config/.env')
 from spoonacular import Spoonacular, RecipeId
 
 # Creating an object
-recipe = Spoonacular("https://www.ricardocuisine.com/en/recipes/5762-classic-beef-chili")
+recipe = Spoonacular("https://www.ricardocuisine.com/en/recipes/5762-classic-beef-chili", allergies='yellow onion')
 
 # Perform steps
 recipe.get_input_recipe()
 recipe.get_ingredients()
 recipe.get_output_recipes(number=20)
-print(recipe.output_recipes[0]['missedIngredients'])
+#print(recipe.output_recipes[0]['missedIngredients'])
 
 recipe.remove_meat_recipes()
 
-print(recipe.output_recipes_veg['output_recipes'])
+print(len(recipe.output_recipes_veg))
+print(recipe.output_recipes_veg[0]['missedIngredients'])
+
+#recipe.remove_allergies()
+
+#print(len(recipe.output_recipes_veg['output_recipes']))
+
+#print(recipe.output_recipes_veg['output_recipes'])
 
 # print(recipe.ingredients)
 # print(recipe.original_df[['aisle', 'name', 'original']])
